8a22344024f5a8b857b0eb1c775fc1c635e116d7
Naidovich Maxim <max.naid@mail.ru>
2016-11-23 19:58:41 +0500

    Добавил лог

--- desktop/src/com/skazki96/kolobok/desktop/DesktopLauncher.java
+++ desktop/src/com/skazki96/kolobok/desktop/DesktopLauncher.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.skazki96.kolobok.MyGdxGame;
 
+import sun.rmi.runtime.Log;
+
+import static com.skazki96.kolobok.MyGdxGame.MAXIM_LOG;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyGdxGame(), config);
+		System.out.println(MAXIM_LOG + " desktop is running");
 	}
 }
