3bd0719494d6af665ff8975578e6204589302f64
Naidovich Maxim <max.naid@mail.ru>
2016-11-23 19:48:50 +0500

    Пустой проект. Лаунчеры к Android, Win, Html, ios

---
+++ desktop/src/com/skazki96/kolobok/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.skazki96.kolobok.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.skazki96.kolobok.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
