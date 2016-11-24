3ca71b6b2d239b0a9c67d96ee882b4c661ed03e0
Naidovich Maxim <max.naid@mail.ru>
2016-11-24 20:04:54 +0500

    Merge branch 'Vens' of https://github.com/NaMax66/Kolobok into Maxim
    # Conflicts:
    #	desktop/src/com/skazki96/kolobok/desktop/DesktopLauncher.java
    
    Я не понял что за конфликты сверху но пока оставил
    
    Добавил меню
    Колобок может ходить

--- desktop/src/com/skazki96/kolobok/desktop/DesktopLauncher.java
+++ desktop/src/com/skazki96/kolobok/desktop/DesktopLauncher.java
@@ -2,18 +2,24 @@ package com.skazki96.kolobok.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.skazki96.kolobok.MyGdxGame;
+import com.skazki96.kolobok.KolobokGame;
 
 //import sun.rmi.runtime.Log;
 
-import static com.skazki96.kolobok.MyGdxGame.MAXIM_LOG;
-import static com.skazki96.kolobok.MyGdxGame.VENS_LOG;
+import static com.skazki96.kolobok.KolobokGame.MAXIM_LOG;
+import static com.skazki96.kolobok.KolobokGame.VENS_LOG;
 
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+
+		config.title = KolobokGame.APP_NAME;
+		config.width = 800;
+		config.height = 480;
+
+		new LwjglApplication(new KolobokGame(), config);
+
 		System.out.println(MAXIM_LOG + " desktop is running");
 		System.out.println(VENS_LOG + " running");
 	}
